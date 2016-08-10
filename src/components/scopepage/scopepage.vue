3c44c5e33e97aa08a6fb961e75b02eb2674c9125
shilianghong <shiliang.hong01@gmail.com>
2016-08-10 10:06:10 -0700

    -update

---
+++ src/components/scopepage/scopepage.vue
@@ -0,0 +1,15 @@
+<script>
+//    import HeaderComponent from './components/header.vue'
+//    import OtherComponent from './components/other.vue'
+    require('./scopepage.scss')
+    export default{
+        template:require('./scopepage.html'),
+        data(){
+            return{
+                msg:'hello vue'
+            }
+        },
+        components:{
+        }
+    }
+</script>
