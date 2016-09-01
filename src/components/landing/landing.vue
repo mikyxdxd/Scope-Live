a02b1627963db428da968a05d53f38fda02facfe
AlexLi88 <alexlxt88@gmail.com>
2016-09-01 16:26:18 -0700

    landing template

--- src/components/landing/landing.vue
+++ src/components/landing/landing.vue
@@ -1,15 +1,11 @@
 <template>
   <div id="home">
-    <navbar></navbar>
 
-    <section>
-      <intro></intro>
-    </section>
-    <section>
-      <whatisit></whatisit>
-    </section>
-    <s_footer></s_footer>
-
+    <div id="landingcontent_view">
+      <navbar></navbar>
+      <router-view id="routerview"></router-view>
+      <s_footer></s_footer>
+    </div>
   </div>
 </template>
 <script>
@@ -18,39 +14,18 @@
 
   export default{
     ready: function(){
-      this.timeStamp = Date.now();
     },
 
     methods: {
-      searchTag: function(tag){
-        this.$route.router.go({ name: 'search', params: { tag: tag.trim()}});
-      },
-      scrollChange(){
-
-        if($('body').scrollTop() <= $('#intro').height()/3){
-          $('#header').addClass('home')
-        }else{
-          $('#header').removeClass('home')
-        }
-      }
     },
-    props:['timeStamp'],
     data(){
       return{
-        pageSize: 30,
-        tag: ""
       }
     },
     ready(){
-      if(this.$route.path == '/'){
-        $('#header').addClass('home')
-        window.addEventListener("scroll", this.scrollChange);
-      };
     },
     components:{
       navbar: require('./header/header.vue'),
-      intro: require('./intro/intro.vue'),
-      whatisit:require('./whatisit/whatisit.vue'),
       s_footer:require('../footer/footer.vue')
     }
   }
