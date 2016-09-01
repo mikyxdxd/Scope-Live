f5919c93540093e6362319819a8a3a8eb40cdb81
shilianghong <shiliang.hong01@gmail.com>
2016-09-01 10:39:42 -0700

    -update

---
+++ src/components/appcontent/appcontent.vue
@@ -0,0 +1,24 @@
+<script>
+    require('./appcontent.scss')
+    export default{
+        template:require('./appcontent.html'),
+        data(){
+            return{
+              slideout:null
+            }
+        },
+
+      ready(){
+
+
+      },
+      components:{
+        sidemenu:require('../user/sidemenu/sidemenu.vue'),
+        dashboard:require('../user/dashboard/dashboard.vue'),
+        appheader:require('./appheader/appheader.vue')
+      },
+      methods:{
+
+      }
+    }
+</script>
---
+++ src/components/appcontent/appheader/appheader.vue
@@ -0,0 +1,20 @@
+<script>
+    require('./appheader.scss')
+    export default{
+        template:require('./appheader.html'),
+        data(){
+            return{
+            }
+        },
+
+      ready(){
+
+
+      },
+      components:{
+      },
+      methods:{
+
+      }
+    }
+</script>
