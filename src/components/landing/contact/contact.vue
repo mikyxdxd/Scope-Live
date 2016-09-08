461f0a3ad2a2a8defc5cda1d0e55a4cd3199fec8
AlexLi88 <alexlxt88@gmail.com>
2016-09-08 10:49:35 -0700

    update

--- src/components/landing/contact/contact.vue
+++ src/components/landing/contact/contact.vue
@@ -9,6 +9,28 @@
               message: ''
             }
         },
+        methods:{
+          submitForm: function(userName, email, message){
+            this.$http.post('/email',
+              {
+                user: userName,
+                email: email,
+                message: message,
+                option: "NONE"
+              }).then((response) => {
+               if(response.body.err){
+                  //error
+                  toastr.error("Your message sent failed. Please try aging.");
+               }else{
+                  //message sent
+                  toastr.success("Thanks for contacting us. We will contact you as soon as possible.");
+               }
+            }, (response)=>{
+                 //failed
+                 toastr.error("Your message sent failed. Please try aging.");
+            });
+          }
+        },
         components:{
 
         }
