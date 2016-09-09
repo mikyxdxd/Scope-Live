cb272a7f5840a63103b08989827593417a747d89
AlexLi88 <alexlxt88@gmail.com>
2016-09-09 12:04:12 -0700

    feature

--- src/components/landing/feature/featurefloor/featurefloor.vue
+++ src/components/landing/feature/featurefloor/featurefloor.vue
@@ -6,6 +6,15 @@
     },
     components:{
 
-    }
+    },
+    data(){
+      return{
+        pContent: require('../../../../assets/Prescreened-Content.png'),
+        pOptions: require('../../../../assets/Presentation-Options.png'),
+        pScope:   require('../../../../assets/Create-Scopes.png'),
+        pLocation:require('../../../../assets/Location_Search-.png'),
+        pAlbum:   require('../../../../assets/Album-Customization.png')
+      }
+    },
   }
 </script>
